9846a57de2afbb2aebe1e4edabad57ebec75ebbd
Carlos Machado <c.machadopadilla@gmail.com>
2017-02-14 13:55:24 -0500

    maybeCall fix
    adding rest parameters to `maybeCall(fn, ...args)` and replace 'arguments' inside cause not bound to arrow funtions

--- src/function.js
+++ src/function.js
@@ -3,6 +3,6 @@ import _ from 'lodash/fp'
 // Function
 // --------
 // (fn, a, b) -> fn(a, b)
-export const maybeCall = fn => _.isFunction(fn) && fn(..._.slice(arguments, 1))
+export const maybeCall = (fn, ...args) => _.isFunction(fn) && fn(...args)
 // ([f, g]) -> !f(x) && !g(x)
 export const overNone = _.flow(_.overSome, _.negate)
