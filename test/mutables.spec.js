c7987ff2442528e94441ae0f470aa007914642e0
Patrick Caron <pcaron@smartprocure.us>
2017-02-16 23:50:36 +0100

    Get these trailing spaces out of Travis' face :wave:

--- test/mutables.spec.js
+++ test/mutables.spec.js
@@ -25,7 +25,7 @@ describe('Mutable FP Functions', function() {
             b: 3,
             c: 4
         }, {
-            a: 1            
+            a: 1
         })).to.deep.equal({
             a: 1,
             b: 3,
