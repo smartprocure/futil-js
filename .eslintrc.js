4afdf20973060c4876fc6ead3c21b9a7455e348c
Daniel Rodríguez <sadasant@gmail.com>
2017-02-17 15:51:12 +0000

    removing mocha plugin rules not yet available in codacy

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,10 +12,13 @@ module.exports = {
         'mocha/no-skipped-tests':        'error',
         'mocha/handle-done-callback':    'error',
         'mocha/no-global-tests':         'error',
-        'mocha/no-return-and-callback':  'error',
         'mocha/valid-suite-description': 'error',
         'mocha/no-sibling-hooks':        'error',
-        'mocha/no-identical-title':      'warn',
+
+        // These two rules are not yet available in codacy
+        // 'mocha/no-return-and-callback':  'error',
+        // 'mocha/no-identical-title':      'warn',
+
         // Because of should.be.true and so on
         indent:                          ['error', 4]
     }
