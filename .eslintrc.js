ec6a9470f49fecdd558088eede3f7841a66c718f
Daniel Rodríguez <sadasant@gmail.com>
2017-02-21 18:31:24 +0000

    Using lodash-fp, moving mocha eslint rules to the test folder

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,23 +3,30 @@ module.exports = {
     plugins: [
         'mocha'
     ],
-    env: {
-        mocha: true
-    },
+    plugins: [
+        'lodash-fp'
+    ],
     rules: {
-        // Mocha plugin
-        'mocha/no-exclusive-tests':      'error',
-        'mocha/no-skipped-tests':        'error',
-        'mocha/handle-done-callback':    'error',
-        'mocha/no-global-tests':         'error',
-        'mocha/valid-suite-description': 'error',
-        'mocha/no-sibling-hooks':        'error',
-
-        // These two rules are not yet available in codacy
-        // 'mocha/no-return-and-callback':  'error',
-        // 'mocha/no-identical-title':      'warn',
-
-        // Because of should.be.true and so on
-        indent:                          ['error', 4]
+        indent: ['error', 4],
+        'lodash-fp/consistent-compose': 'off',
+        'lodash-fp/consistent-name': [ 'error', '_' ],
+        'lodash-fp/no-argumentless-calls': 'error',
+        'lodash-fp/no-chain': 'error',
+        'lodash-fp/no-extraneous-args': 'error',
+        'lodash-fp/no-extraneous-function-wrapping': 'error',
+        'lodash-fp/no-extraneous-iteratee-args': 'error',
+        'lodash-fp/no-for-each': 'off',
+        'lodash-fp/no-partial-of-curried': 'error',
+        'lodash-fp/no-single-composition': 'error',
+        'lodash-fp/no-submodule-destructuring': 'error',
+        'lodash-fp/no-unused-result': 'error',
+        'lodash-fp/prefer-compact': 'error',
+        'lodash-fp/prefer-composition-grouping': 'error',
+        'lodash-fp/prefer-constant': [ 'error', { arrowFunctions: false } ],
+        'lodash-fp/prefer-flat-map': 'error',
+        'lodash-fp/prefer-get': 'error',
+        'lodash-fp/prefer-identity': [ 'error', { arrowFunctions: false } ],
+        'lodash-fp/preferred-alias': 'off',
+        'lodash-fp/use-fp': 'error'
     }
-};
+}
