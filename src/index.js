52358dfd3c7d1085f8d07887472f2381c660f6b5
Daniel Rodríguez <sadasant@gmail.com>
2017-03-14 05:34:13 +0000

    y-combinating foldWhile

--- src/index.js
+++ src/index.js
@@ -29,18 +29,18 @@ export const map = _.curry((f, x) => (_.isArray(x) ? _.map : _.mapValues)(f, x))
 export const deepMap = _.curry((fn, obj, _map = map, is = isTraversable) =>
     _map(e => is(e) ? deepMap(fn, fn(e), _map, is) : e, obj))
 
+// y-combinator, as in: http://kestas.kuliukas.com/YCombinatorExplained
+// f is the function that returns the function we want to make recursive
+// n indicates the next call of f
+export const y = f => (n => n(n))(n => f(x => n(n)(x))) // eslint-disable-line lodash-fp/no-extraneous-function-wrapping
+
 // Like haskell's: fold takeWhile
 /* eslint-disable lodash-fp/no-chain */
-export const foldWhile = _.curry((fn, obj, acc = []) => {
-    let inFold = o =>
-        _.isObject(o)
-        ? _(o).toPairs().every(a => fn(acc, a[1], a[0]) && inFold(a[1]))
-        : _.isArray(o)
-        ? _(o).every(e => fn(acc, e) && inFold(e))
-        : o
-    inFold(obj)
-    return acc
-})
+export const foldWhile = _.curry((fn, obj, r = []) =>
+    y(next => o => isTraversable(o)
+    ? _.every(k => fn(r, o[k], k) && next(o[k]), _.keys(o))
+    : o)(obj)
+    ? r : r)
 /* eslint-enable lodash-fp/no-chain */
 
 // Queries a traversable data structure
