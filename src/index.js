bd8098bf728fc22aa7df9248c49997640681c857
Daniel Rodríguez <sadasant@gmail.com>
2017-03-18 04:34:40 +0000

    comments

--- src/index.js
+++ src/index.js
@@ -33,24 +33,42 @@ export const deepMap = _.curry((fn, obj, _map = map, is = isTraversable) =>
 // non-deterministic input iterators over discrete state changes
 // aka groupoid category
 // See: https://en.m.wikipedia.org/wiki/Automata_theory#Connection_to_category_theory
+// How to use it:
+//   groupoid(reducer1, reducer2, ...reducerN)(field, [optional params, see below])
+// Each reducer will receive:
+//   - The result of the previous reducer
+//   - The current value
+//   - The key where the current value is located
+//   - The key names of the parents of the current key
 export const groupoid = (...funs) => function G (
-    field,
-    acc = [],
-    breadth,
-    orientation = 1,
-    path = []
+    // REQUIRED
+    field, // input n-dimensional field
+    // Optional parameters
+    acc = [], // accumulator
+    breadth, // breadth first is falsy by default
+    orientation = 1, // > 0 by default, goes bottom to top if < 0
+    path = [] // only used for recursion purposes
 ) {
+    // return the current accumulator if the field is not iterable
     if (!field || typeof field !== 'object') return acc
+
     let accepted = acc
     let state = acc
     let keys = Object.keys(field)
-    if (orientation < 0) keys = keys.reverse()
-    let key = keys.shift()
     let fN = 0
     let nextBreadth = []
+
+    if (orientation < 0) keys = keys.reverse()
+    let key = keys.shift()
+
+    // stops only if the state resulting from funs[fN] is exactly falsee
+    // (or if (!key) break)
     while (state !== false) {
         accepted = state
         let f = funs[fN]
+        // if we have no more functins to look at,
+        // get the next key and reset the function
+        // counter. Lets us avoid having two whiles.
         if (!f) {
             key = keys.shift()
             fN = 0
@@ -58,7 +76,11 @@ export const groupoid = (...funs) => function G (
         }
         if (!key) break
         let val = field[key]
+        // result of reducer f[fN]
         let result = f(state, val, key, path)
+        // Either we accumulate iterables to go deeper
+        // after all the current level (breadth),
+        // or we go recursive right now.
         if (breadth && val && typeof val === 'object') {
             nextBreadth.push({ val, key })
         } else {
@@ -67,6 +89,7 @@ export const groupoid = (...funs) => function G (
         if (result !== true) state = result
         fN++
     }
+    // if breadth first, going recursive on what was nested in this layer
     if (nextBreadth.length) {
         accepted = [accepted].concat(nextBreadth).reduce((a, { val, key }) => {
             let result = G(val, a, breadth, orientation, path.concat(key))
