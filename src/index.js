b17810ff763714f7faf5d67a47af80e7e6622b07
Carlos Machado <c.machadopadilla@gmail.com>
2017-03-24 11:04:06 -0400

    add matchAnyWord/makeAndTest/makeRegex
    add matchAnyWord and utility functions makeAndTest/makeRegex

--- src/index.js
+++ src/index.js
@@ -32,3 +32,10 @@ export const deepMap = _.curry((fn, obj, _map = map, is = isTraversable) =>
 // Misc
 // ----
 export const testRegex = regex => regex.test.bind(regex)
+export const makeRegex = options => text => RegExp(text, options)
+export const makeAndTest = options => _.flow(makeRegex(options), testRegex)
+export const matchAnyWord = _.flow(
+  _.words,
+  _.map(makeAndTest('gi')),
+  _.overSome
+)
