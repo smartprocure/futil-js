260c52c0cf409e991ab1d5f004f02c5b77a2afa7
Carlos Machado <c.machadopadilla@gmail.com>
2017-03-24 14:40:48 -0400

    Update misc.spec.js

--- test/misc.spec.js
+++ test/misc.spec.js
@@ -40,4 +40,29 @@ describe('Regexp Functions', () => {
         expect(f.testRegex(/smart/i)('SmartProcure')).to.equal(true)
         expect(f.testRegex(/smart/)('SmartProcure')).to.equal(false)
     })
+    
+    it('makeRegExp', () => {
+      const reText = 'Some text';
+      const options = 'gi';
+
+      expect(f.makeRegex(options)(reText)).to.deep.equal(RegExp(reText, options))
+    })
+
+    it('makeAndTest', () => {
+      const reText = 'Some text';
+      const options = 'gi';
+      const text = "Here is some text to test"
+      const regex = RegExp(reText, options)
+
+      expect(f.makeAndTest(options)(reText)(text)).to.deep.equal(regex.test(text))
+    })
+
+    it('matchAnyWord', () => {
+      const reText = 'Some text';
+      const options = 'gi';
+      const text = "Here is some text to test"
+      const regex = RegExp(reText, options)
+      const match = f.matchAnyWord(reText)
+      expect(match(text)).to.equal(true)
+    })
 })
