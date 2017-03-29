497b75f60f8b20d8ebe976b848e4f93fb017345d
Samuel Greene <sam@samuelgreene.info>
2017-03-29 11:20:03 -0400

    make merge ranges more legible

--- src/array.js
+++ src/array.js
@@ -7,4 +7,8 @@ export const dotJoin = compactJoin('.')
 export const repeated = _.flow(_.groupBy(e => e), _.filter(e => e.length > 1), _.flatten, _.uniq)
 export const push = (val, arr) => arr.concat([val])
 export const insertAtIndex = (index, val, str) => str.slice(0, index) + val + str.slice(index)
-export const mergeRanges = (p1, p2) => ((p2[0] <= p1[1]) && [[p1[0], _.max([p1[1], p2[1]])]]) || [p1, p2]
+
+let overlaps = (x, y) =>  y[0] > x[1]
+let mergeRange = (x, y) => [[x[0], _.max(x.concat(y))]]
+export const mergeRanges = (x, y) => overlaps(x, y) ? [x, y] : mergeRange(x, y)
+
