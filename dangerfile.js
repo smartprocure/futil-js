6948ea709700afa14c6e04e39e29ac5a621efcae
Daniel Rodríguez <sadasant@gmail.com>
2017-03-31 22:46:03 +0000

    Adding change to changelog and package.json
    Added danger ⚠️

---
+++ dangerfile.js
@@ -0,0 +1,34 @@
+import { danger, fail, warn } from 'danger'
+import _ from 'lodash/fp'
+
+const changed = {
+  changelog: _.includes(danger.git.modified_files, 'CHANGELOG.md'),
+  packageJSON: _.includes(danger.git.modified_files, 'package.json')
+}
+
+// No PR is too small to warrant a paragraph or two of summary
+if (danger.github.pr.body.length === 0) {
+  fail('Please add a description to your PR.')
+}
+
+// Always ensure we assign someone, so that our Slackbot can do its work correctly
+if (danger.github.pr.assignee === null) {
+  fail('Please assign someone to merge this PR, and optionally include people who should review.')
+}
+
+// Requires CHANGELOG entries
+if (!changed.changelog) {
+  fail('Please add a changelog entry for your changes.')
+}
+
+// Requires a version update in package.json
+const packageDiff = danger.git.diffForFile('package.json')
+if (!changed.packageJSON && packageDiff.includes('version')) {
+  fail('Please bump up the version')
+}
+
+// Warns when PR size is large
+const bigPRThreshold = 600
+if (danger.github.pr.additions + danger.github.pr.deletions > bigPRThreshold) {
+  warn(`:exclamation: This PR is BIG (+${danger.github.pr.additions} -${danger.github.pr.deletions})`)
+}
