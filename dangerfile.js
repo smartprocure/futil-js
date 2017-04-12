e4af1652562d3e3ee18418ce50e8ebc3a4845402
Daniel Rodríguez <sadasant@gmail.com>
2017-04-12 13:38:16 +0000

    fixing typo with modified_files and adding logging in dangerfile.js

--- dangerfile.js
+++ dangerfile.js
@@ -1,34 +1,36 @@
 import { danger, fail, warn } from 'danger'
 import _ from 'lodash/fp'
 
+const log = (f, s) => console.log(f.name, s) & f(s)
+
 const changed = {
-  changelog: _.includes('CHANGELOG.md', danger.git.modified_file),
+  changelog: _.includes('CHANGELOG.md', danger.git.modified_files),
   packageJSON: _.includes('package.json', danger.git.modified_files)
 }
 
 // No PR is too small to warrant a paragraph or two of summary
 if (danger.github.pr.body.length === 0) {
-  fail('Please add a description to your PR.')
+  log(fail, 'Please add a description to your PR.')
 }
 
 // Always ensure we assign someone, so that our Slackbot can do its work correctly
 if (danger.github.pr.assignee === null) {
-  fail('Please assign someone to merge this PR, and optionally include people who should review.')
+  log(fail, 'Please assign someone to merge this PR, and optionally include people who should review.')
 }
 
 // Requires CHANGELOG entries
 if (!changed.changelog) {
-  fail('Please add a changelog entry for your changes.')
+  log(fail, 'Please add a changelog entry for your changes.')
 }
 
 // Requires a version update in package.json
 const packageDiff = danger.git.diffForFile('package.json')
 if (!changed.packageJSON && packageDiff.includes('version')) {
-  fail('Please bump up the version')
+  log(fail, 'Please bump up the version')
 }
 
 // Warns when PR size is large
 const bigPRThreshold = 600
 if (danger.github.pr.additions + danger.github.pr.deletions > bigPRThreshold) {
-  warn(`:exclamation: This PR is BIG (+${danger.github.pr.additions} -${danger.github.pr.deletions})`)
+  log(warn, `:exclamation: This PR is BIG (+${danger.github.pr.additions} -${danger.github.pr.deletions})`)
 }
