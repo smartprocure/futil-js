1bfba6793bfe940da6468cda283fbaf970414c0d
Samuel Greene <sgreene@smartprocure.us>
2017-07-31 04:27:47 +0200

    add support for aspect always, names, and bug fix for processing sample aspect

--- src/aspect.js
+++ src/aspect.js
@@ -1,50 +1,67 @@
 import _ from 'lodash/fp'
-import {defaultsOn} from './conversion'
+import {defaultsOn, setOn} from './conversion'
 import {throws} from './index'
 
 // Core
 export let aspect = ({
+  name = 'aspect',
   init = _.noop,
   after = _.noop,
   before = _.noop,
+  always = _.noop,
   onError = throws
   // ?: interceptParams, interceptResult, wrap
 }) => f => {
   let {state = {}} = f
   init(state)
-  let result = (...args) => {
-    let result
-    return Promise.resolve()
-      .then(() => before(args, state))
-      .then(() => f(...args))
-      .then(r => {
-        result = r
-      })
-      .then(() => after(result, state, args))
-      .then(() => result)
-      .catch(e => onError(e, state, args))
+  // Trick to set function.name of anonymous function
+  let x = {
+    [name]: (...args) => {
+      let result
+      let error
+      return Promise.resolve()
+        .then(() => before(args, state))
+        .then(() => f(...args))
+        .then(r => {
+          result = r
+        })
+        .then(() => after(result, state, args))
+        .catch(e => onError(e, state, args))
+        .catch(e => {
+          error = e
+        })
+        .then(() => always(state, args))
+        .then(() => {
+          if (error) throw error
+        })
+        .then(() => result)
+    }
   }
-  result.state = state
-  return result
+  x[name].state = state
+  return x[name]
 }
 
 // Example Aspects
 let logs = (extend = defaultsOn) => aspect({
   init: extend({ logs: [] }),
-  after: (result, state) => state.logs.push(result)
+  after: (result, state) => state.logs.push(result),
+  name: 'logs'
 })
 let error = (extend = defaultsOn) => aspect({
   init: extend({ error: null }),
-  onError: (e, state) => { state.error = e }
+  onError: setOn('error'),
+  name: 'error'
 })
 let errors = (extend = defaultsOn) => aspect({
   init: extend({ errors: [] }),
-  onError: (e, state) => state.errors.push(e)
+  onError: (e, state) => state.errors.push(e),
+  name: 'errors'
 })
 let status = (extend = defaultsOn) => aspect({
   init: extend({ processing: false }),
   before: (params, state) => { state.processing = true },
-  after: (result, state) => { state.processing = false }
+  always: setOn('processing', false),
+  name: 'status'
 })
 // This is a function just for consistency
 let concurrency = () => aspect({
@@ -54,7 +71,8 @@ let concurrency = () => aspect({
         message: 'Concurrent Runs Not Allowed'
       })
     }
-  }
+  },
+  name: 'concurrency'
 })
 
 export let aspects = {
