193f474ed7f4d016853bd5f39eda33be9f7f2f73
Samuel Greene <sgreene@smartprocure.us>
2017-07-31 21:24:20 +0200

    add tapError

--- src/lang.js
+++ src/lang.js
@@ -1,6 +1,10 @@
 import _ from 'lodash/fp'
 
 export let throws = x => { throw x }
+export let tapError = f => (e, ...args) => {
+  f(e, ...args)
+  throw e
+}
 export let isNotNil = _.negate(_.isNil)
 export let exists = isNotNil
 export let isMultiple = x => (x || []).length > 1
