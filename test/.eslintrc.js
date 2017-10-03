aa3d509ac2650da50bac3e9a9df7e8e73d07bbae
Sean Clayton <clayton.sean@gmail.com>
2017-10-03 15:14:40 -0400

    Add eslint config

--- test/.eslintrc.js
+++ test/.eslintrc.js
@@ -1,18 +1,5 @@
 module.exports = {
-  'extends': 'standard',
-  plugins: [
-    'mocha'
-  ],
   env: {
     mocha: true
-  },
-  rules: {
-    'no-unused-expressions': 'warn',
-    'mocha/no-exclusive-tests': 'error',
-    'mocha/no-skipped-tests': 'error',
-    'mocha/handle-done-callback': 'error',
-    'mocha/no-global-tests': 'error',
-    'mocha/valid-suite-description': 'error',
-    'mocha/no-sibling-hooks': 'error'
   }
 }
