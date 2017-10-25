1fb974df4d871d59222c8c5705ec7ae9fa96da30
Marc Liu <m@liu.pm>
2017-10-25 11:56:09 -0700

    Increase the timeout/delay to make testing IE9/10/11 more stable.

--- karma.conf.js
+++ karma.conf.js
@@ -195,11 +195,13 @@ var customLaunchers = {
   sl_ie_9: {
     base: 'SauceLabs',
     browserName: 'internet explorer',
+    platform: 'Windows 7',
     version: '9',
   },
   sl_ie_10: {
     base: 'SauceLabs',
     browserName: 'internet explorer',
+    platform: 'Windows 8',
     version: '10',
   },
   sl_ie_11: {
