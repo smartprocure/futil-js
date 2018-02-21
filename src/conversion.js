70e8695d29f2615ac971700c24e355d4df8d024c
Decrapifier <drodriguez@smartprocure.us>
2018-02-21 00:23:20 +0000

    Automagically formatted by Duti!
    https://github.com/smartprocure/duti

--- src/conversion.js
+++ src/conversion.js
@@ -23,17 +23,20 @@ export const setOn = mutable.set
 export const unsetOn = _.curryN(2, mutable.unset)
 export const pullOn = mutable.pull
 
-
 // Uncaps
 // ------
 // Un-prefixed Deprecated
-export const reduce = aspects.deprecate('reduce', '1.28.0', 'reduceIndexed')(noCap.reduce)
+export const reduce = aspects.deprecate('reduce', '1.28.0', 'reduceIndexed')(
+  noCap.reduce
+)
 export const mapValues = aspects.deprecate(
   'mapValues',
   '1.28.0',
   'mapValuesIndexed'
 )(noCap.mapValues)
-export const each = aspects.deprecate('each', '1.28.0', 'eachIndexed')(noCap.each)
+export const each = aspects.deprecate('each', '1.28.0', 'eachIndexed')(
+  noCap.each
+)
 
 export const mapIndexed = noCap.map
 export const findIndexed = noCap.find
