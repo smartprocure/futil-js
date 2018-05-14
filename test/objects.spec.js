2397ee204ff787aa497f3941bf721dee0b3d636d
Sten Muchow <sten.muchow@gmail.com>
2018-05-14 17:33:14 +0100

    updated unit test to use lodash update

--- test/objects.spec.js
+++ test/objects.spec.js
@@ -1,4 +1,5 @@
 import chai from 'chai'
+import _ from 'lodash';
 import * as f from '../src'
 
 chai.expect()
@@ -181,7 +182,7 @@ describe('Object Functions', () => {
     expect(f.compareDeep('a.b.c')(o)('1')).to.deep.equal(false)
   })
   it('mapProp', () => {
-    const a = f.mapProp(val => val * val, 'a', { a: 2, b: 1 })
+    const a = _.update(val => val * val, 'a', { a: 2, b: 1 })
     expect(a).to.deep.equal({ a: 4, b: 1 })
   })
   it('getOrReturn', () => {
