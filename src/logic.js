841df2ce24a451aa1911dfb45763a6e5b1e7e631
Decrapifier <decrapifier@govspend.com>
2019-09-11 20:53:40 +0000

    Automagically formatted by Duti!
    https://github.com/smartprocure/duti

--- src/logic.js
+++ src/logic.js
@@ -15,8 +15,12 @@ export let ifElse = _.curry((condition, onTrue, onFalse, x) =>
     ? callOrReturn(onTrue, x)
     : callOrReturn(onFalse, x)
 )
-export let when = _.curry((condition, t, x) => ifElse(condition, t, _.identity, x))
-export let unless = _.curry((condition, f, x) => ifElse(condition, _.identity, f, x))
+export let when = _.curry((condition, t, x) =>
+  ifElse(condition, t, _.identity, x)
+)
+export let unless = _.curry((condition, f, x) =>
+  ifElse(condition, _.identity, f, x)
+)
 
 export let whenExists = when(exists)
 export let whenTruthy = when(Boolean)
