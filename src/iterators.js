e08048e801fdb8ca3210e6adceadc46aa2086ef3
bampowbam <37565825+bampowbam@users.noreply.github.com>
2020-02-25 21:42:54 -0600

    Update iterators.js
    Update this file with new function differentIndex

--- src/iterators.js
+++ src/iterators.js
@@ -4,3 +4,9 @@ export let differentLast = (normalCase, lastCase) => (acc, i, list) =>
   i === list.length - 1
     ? _.iteratee(lastCase)(acc, i, list)
     : _.iteratee(normalCase)(acc, i, list)
+
+export let differentIndex = (normalCase, indexCase, index) =>
+(acc, i) =>
+  i === index
+    ? _.iteratee(indexCase)(acc, i)
+    : _.iteratee(normalCase)(acc, i);
