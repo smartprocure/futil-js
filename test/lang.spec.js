5486faadc1b2118191769ff80f7facc052d97c1d
Nicolas Olivares <nolivaresr@hotmail.com>
2022-08-12 14:52:06 -0400

    almost done

--- test/lang.spec.js
+++ test/lang.spec.js
@@ -1,65 +1,101 @@
 import chai from 'chai'
-import * as f from '../src'
+import * as F from '../src'
 import _ from 'lodash/fp'
 chai.expect()
 const expect = chai.expect
 
 describe('Lang Functions', () => {
   it('throws', () => {
-    expect(() => f.throws(Error('oops'))).to.throw()
+    expect(() => F.throws(Error('oops'))).to.throw('oops')
+  })
+  it('tapError', () => {
+    let errorFn = (e, preCode, postCode) => console.log(`${e} with code ${preCode}${postCode}`)
+    let errorOfMine = new Error('myError')
+    try {
+      // It will print: 'Error: myError with code 2045'
+      // Then it will throw error
+      expect(F.tapError(errorFn)(errorOfMine, 20, 45)).to.throw('myError')
+    } catch (e) {
+      expect(e).to.equal(errorOfMine)
+    }
+    
   })
   it('isNotNil', () => {
-    expect(f.isNotNil(null)).to.equal(false)
-    expect(f.isNotNil(undefined)).to.equal(false)
-    expect(f.isNotNil(0)).to.equal(true)
-    expect(f.isNotNil('')).to.equal(true)
-    expect(f.isNotNil([])).to.equal(true)
+    expect(F.isNotNil(null)).to.equal(false)
+    expect(F.isNotNil(undefined)).to.equal(false)
+    expect(F.isNotNil(0)).to.equal(true)
+    expect(F.isNotNil('')).to.equal(true)
+    expect(F.isNotNil([])).to.equal(true)
+    expect(F.isNotNil).to.equal(F.exists)
   })
   it('exists', () => {
-    expect(f.exists).to.equal(f.isNotNil)
+    expect(F.exists(null)).to.equal(false)
+    expect(F.exists(undefined)).to.equal(false)
+    expect(F.exists(0)).to.equal(true)
+    expect(F.exists('')).to.equal(true)
+    expect(F.exists([])).to.equal(true)
+    expect(F.exists).to.equal(F.isNotNil)
   })
   it('isMultiple', () => {
-    expect(f.isMultiple([''])).to.equal(false)
-    expect(f.isMultiple(['', ''])).to.equal(true)
-    expect(f.isMultiple('a')).to.equal(false)
-    expect(f.isMultiple('asdf')).to.equal(true)
-    expect(f.isMultiple({ x: 1, y: 2 })).to.equal(false)
-    expect(f.isMultiple({ x: 1, y: 2, length: 2 })).to.equal(true)
+    expect(F.isMultiple([''])).to.equal(false)
+    expect(F.isMultiple(['', ''])).to.equal(true)
+    expect(F.isMultiple('a')).to.equal(false)
+    expect(F.isMultiple('asdf')).to.equal(true)
+    expect(F.isMultiple({ x: 1, y: 2 })).to.equal(false)
+    expect(F.isMultiple({ x: 1, y: 2, length: 2 })).to.equal(true)
   })
   it('append', () => {
-    expect(f.append('a', 'b')).to.equal('ba')
-    expect(f.append(1, 4)).to.equal(5)
+    expect(F.append('a', 'b')).to.equal('ba')
+    expect(F.append(1, 4)).to.equal(5)
   })
   it('isBlank', () => {
-    expect(f.isBlank(1)).to.equal(false)
-    expect(f.isBlank('asdf')).to.equal(false)
-    expect(f.isBlank({ a: 1 })).to.equal(false)
-    expect(f.isBlank([3, 4])).to.equal(false)
-    expect(f.isBlank(new Date())).to.equal(false)
+    expect(F.isBlank(1)).to.equal(false)
+    expect(F.isBlank('asdf')).to.equal(false)
+    expect(F.isBlank({ a: 1 })).to.equal(false)
+    expect(F.isBlank([3, 4])).to.equal(false)
+    expect(F.isBlank(new Date())).to.equal(false)
     expect(
-      f.isBlank({
+      F.isBlank({
         a: 1,
         b: 'as',
       })
     ).to.equal(false)
-    expect(f.isBlank(null)).to.equal(true)
-    expect(f.isBlank(undefined)).to.equal(true)
-    expect(f.isBlank('')).to.equal(true)
-    expect(f.isBlank([])).to.equal(true)
-    expect(f.isBlank({})).to.equal(true)
+    expect(F.isBlank(null)).to.equal(true)
+    expect(F.isBlank(undefined)).to.equal(true)
+    expect(F.isBlank('')).to.equal(true)
+    expect(F.isBlank([])).to.equal(true)
+    expect(F.isBlank({})).to.equal(true)
   })
-  it('should isBlankDeep', () => {
-    expect(f.isBlankDeep(_.every)(1)).to.equal(false)
-    expect(f.isBlankDeep(_.every)(false)).to.equal(false)
-    expect(f.isBlankDeep(_.every)('')).to.equal(true)
+  it('isNotBlank', () => {
+    expect(F.isNotBlank(1)).to.equal(true)
+    expect(F.isNotBlank('asdf')).to.equal(true)
+    expect(F.isNotBlank({ a: 1 })).to.equal(true)
+    expect(F.isNotBlank([3, 4])).to.equal(true)
+    expect(F.isNotBlank(new Date())).to.equal(true)
     expect(
-      f.isBlankDeep(_.every)({
+      F.isNotBlank({
+        a: 1,
+        b: 'as',
+      })
+    ).to.equal(true)
+    expect(F.isNotBlank(null)).to.equal(false)
+    expect(F.isNotBlank(undefined)).to.equal(false)
+    expect(F.isNotBlank('')).to.equal(false)
+    expect(F.isNotBlank([])).to.equal(false)
+    expect(F.isNotBlank({})).to.equal(false)
+  })
+  it('isBlankDeep', () => {
+    expect(F.isBlankDeep(_.every)(1)).to.equal(false)
+    expect(F.isBlankDeep(_.every)(false)).to.equal(false)
+    expect(F.isBlankDeep(_.every)('')).to.equal(true)
+    expect(
+      F.isBlankDeep(_.every)({
         a: 1,
         b: 'as',
       })
     ).to.equal(false)
     expect(
-      f.isBlankDeep(_.every)({
+      F.isBlankDeep(_.every)({
         a: null,
         b: '',
         c: [],
