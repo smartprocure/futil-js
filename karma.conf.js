ebe00645618863d341f2e8dcc9abc2aae116979a
Henry Thornley <hthornley@govspend.com>
2022-10-07 15:02:12 -0700

    Comment Out Karma
    - Add configs back and remove from CI pipeline.

--- karma.conf.js
+++ karma.conf.js
@@ -125,4 +125,70 @@ module.exports = function (config) {
 // Browsers to run on Sauce Labs
 // Check out https://saucelabs.com/platforms for all browser/OS combos
 // UPDATED to match angular's config https://github.com/angular/angular.js/blob/master/karma-shared.conf.js
-let customLaunchers = {}
+let customLaunchers = {
+  SL_Chrome: {
+    base: "SauceLabs",
+    browserName: "chrome",
+    version: "latest",
+  },
+  "SL_Chrome-1": {
+    base: "SauceLabs",
+    browserName: "chrome",
+    version: "latest-1",
+  },
+  SL_Firefox: {
+    base: "SauceLabs",
+    browserName: "firefox",
+    version: "latest",
+  },
+  "SL_Firefox-1": {
+    base: "SauceLabs",
+    browserName: "firefox",
+    version: "latest-1",
+  },
+  "SL_Safari-1": {
+    base: "SauceLabs",
+    browserName: "safari",
+    version: "latest-1",
+  },
+  SL_Safari: {
+    base: "SauceLabs",
+    browserName: "safari",
+    version: "latest",
+  },
+  // 'SL_IE_9': {
+  //   base: 'SauceLabs',
+  //   browserName: 'internet explorer',
+  //   platform: 'Windows 2008',
+  //   version: '9'
+  // },
+
+  SL_IE_11: {
+    base: "SauceLabs",
+    browserName: "internet explorer",
+    platform: "Windows 8.1",
+    version: "11",
+  },
+  SL_EDGE: {
+    base: "SauceLabs",
+    browserName: "microsoftedge",
+    platform: "Windows 10",
+    version: "latest",
+  },
+  "SL_EDGE-1": {
+    base: "SauceLabs",
+    browserName: "microsoftedge",
+    platform: "Windows 10",
+    version: "latest-1",
+  },
+  SL_iOS: {
+    base: "SauceLabs",
+    browserName: "iphone",
+    version: "latest",
+  },
+  "SL_iOS-1": {
+    base: "SauceLabs",
+    browserName: "iphone",
+    version: "latest-1",
+  },
+}
